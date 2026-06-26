1b62a757549482a189d674d773e27ca2a2d0413d
hankyboy123abc <hank60471@gmail.com>
2026-06-26 12:30:16 -0400

    index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ http.createServer((req, res) => {
 }).listen(process.env.PORT || 3000);
 
 const bot = mineflayer.createBot({
-  host: 'YOUR_SERVER_IP',        // Change to your server's IP address
+  host: 'donutsmp.net',        // Change to your server's IP address
   port: 25565,                   // Change if your server uses a different port
-  username: 'YOUR_MICROSOFT_EMAIL', // Your official Minecraft email
+  username: 'hank60471@gmail.com', // Your official Minecraft email
   auth: 'microsoft'              // Securely connects to your Microsoft account
 });
 
